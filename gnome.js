ca0c512d196a7f7a0c19c20e0d84652fa3f3c733
AsalaKM <asalamatarya@gmail.com>
2018-08-14 16:09:34 +0300

    group4

---
+++ gnome.js
@@ -0,0 +1,34 @@
+
+/**
+ * Genome function is a function that descending sort an array by swaping 
+ * the array elements.
+ * 
+ * example:
+ * 
+ * gnomeSort([3, 0, 2, 5, -1, 4, 1])
+ * 
+ * the result 
+ * // => [ -1, 0, 1, 2, 3, 4, 5 ]
+ */
+
+
+function gnomeSort(arr) {
+
+    // loop over the arry and check the which element is the smalest 
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i - 1] > arr[i]) swap(arr, i);
+    }
+    return arr;
+}
+
+
+function swap(arr, i) {
+
+    // loop over the two element and made a swap
+    for (; i > 0 && arr[i - 1] > arr[i]; i--) {
+        var temp = arr[i];
+        arr[i] = arr[i - 1];
+        arr[i - 1] = temp;
+    }
+}
+console.log(gnomeSort([3, 0, 2, 5, -1, 4, 1]));
